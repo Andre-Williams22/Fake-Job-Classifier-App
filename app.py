705b7672ebc4582bf806c401d0f548b80be90b41
Andre Williams <awjrs22@gmail.com>
2020-04-19 19:37:00 -0700

    code decision tree code works and model can run on a flask app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sklearn
 app = Flask(__name__)
 
 #model = pickle.load(open('random_forest_model_1.pkl', 'rb'))
+model = pickle.load(open('example_dtclf_model.pkl', 'rb'))
 
 # Home Route
 @app.route('/')
@@ -20,6 +21,13 @@ def predict():
     final = [np.array(int_features)]
     prediction = model.predict_proba(final)
     output = '{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output == 1.00:
+        yes = 'yes'
+        return render_template('base.html', yes=yes)
+    else:
+        no = 'no'
+        return render_template('base.html', no=no)
 
 
 if __name__ == '__main__':
